9e50db18a599948f42a6970a9ec7eebc07c73bb7
root <maquanyi@huawei.com>
2015-08-05 00:24:16 +0800

    Change domain to domains in setting

--- setting/setting.go
+++ setting/setting.go
@@ -132,7 +132,7 @@ func SetConfig(path string) error {
 		err = fmt.Errorf("Image path config value is null")
 	}
 
-	if domains := conf.String("dockyard::domain"); domains != "" {
+	if domains := conf.String("dockyard::domains"); domains != "" {
 		Domains = domains
 	} else if domains == "" {
 		err = fmt.Errorf("Domains value is null")
